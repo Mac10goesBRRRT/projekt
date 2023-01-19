c135c13e97a823e4b437375f981740b63da7c8c9
Ronja Awe <ronja.awe@informatik.hs-fulda.de>
2023-01-19 13:26:20 +0100

    refactoring: userausgabe und arrays geändert

--- test/c/test_raetselronja.c
+++ test/c/test_raetselronja.c
@@ -21,11 +21,12 @@ void test_runRaetselTest(void){
     int result;
     int input[] = {0,1,2,3};
     int loesung = 4;
+    int n = 4;
 
     /* act */
     //Die Funktion wird ausgeführt
     userInput_ExpectAndReturn(4);
-    result = zahlenraetsel(input, loesung);
+    result = zahlenraetsel(input, loesung, n);
 
     /* assert */
     //Vergleichen
@@ -37,15 +38,16 @@ void test_runRaetselTestFalsch(void){
 int result;
 int input[] = {0,1,2,3};
 int loesung = 4;
+int n = 4;
 
 userInput_ExpectAndReturn(5);
-result = zahlenraetsel(input, loesung);
+result = zahlenraetsel(input, loesung, n);
 
 TEST_ASSERT_EQUAL_INT(0, result);
 
 }
 
-void test_runRaetselTestzwei(void){
+void test_runRaetselTestrichrig(void){
 
 userInput_ExpectAndReturn(198);
 int result = raetselaufruf(1); //hier wird rätsel 1 aufgerufen
